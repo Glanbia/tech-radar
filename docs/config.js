7e2d26179b9ba3202a4218902b668116219964c0
Sujesh Menon <smenon@glanbia.ie>
2024-11-10 02:10:04 +0000

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -3,7 +3,7 @@ radar_visualization({
     width: 1450,
     height: 1000,
     colors: {
-        background: "#14141f",
+        background: "#292524",
         grid: '#999',
         inactive: "#ddd"
     },
